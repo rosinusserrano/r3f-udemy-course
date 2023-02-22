ad7c61eec73426ed0ca6b5548d5431fe273153e1
Marco Rosinus Serrano <57681190+rosinusserrano@users.noreply.github.com>
2023-02-22 10:58:59 +0000

    lesson 3 (props)

---
+++ props/src/Box.jsx
@@ -0,0 +1,8 @@
+export default function Box(props) {
+    return (
+      <mesh {...props}>
+        <boxGeometry />
+        <meshBasicMaterial color={0x00ff00} wireframe/>
+      </mesh>
+    )
+  }
