c64e2ed999d70166eca2742248006c59381c7066
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2023-02-26 14:24:17 +0100

    started lesson 11

---
+++ 11_stats-panel/src/App.jsx
@@ -0,0 +1,22 @@
+import { Canvas } from '@react-three/fiber'
+import Polyhedron from './Polyhedron'
+import * as THREE from 'three'
+import { Stats } from '@react-three/drei'
+
+
+export default function App() {
+  const polyhedron = [
+    new THREE.BoxGeometry(),
+    new THREE.SphereGeometry(0.785398),
+    new THREE.DodecahedronGeometry(0.785398),
+  ]
+
+  return (
+    <Canvas camera={{ position: [0, 0, 3] }}>
+      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+    </Canvas>
+  )
+}
